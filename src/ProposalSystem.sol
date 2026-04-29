b026ed40b4365969341f5e67179ba550844e55d5
RonakGohel <ronakgohel2525@gmail.com>
2026-04-29 17:37:32 +0000

    NATSPEC Added

--- src/ProposalSystem.sol
+++ src/ProposalSystem.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.33;
 
 /// @title Voting And Delegation Systen
 /// @author Ronak Gohel
-/// @notice People can make proposals, users can vote for proposals, and even delegate the
+/// @notice People can make proposals, users can vote for proposals, and even delegate their votes
 contract ProposalSystem {
     uint256 private sIndexNumber;
 
@@ -13,10 +13,13 @@ contract ProposalSystem {
     string[] private sProposals;
     uint256[] private sIndexNo;
 
+    /// Sets Index Number to 1
     constructor() {
         sIndexNumber = 1;
     }
 
+    /// User can call this function and submit their proposal
+    /// @param proposal Submit Proposal
     function makeProposal(string memory proposal) external {
         sProposalToIndexNumber[proposal] = sIndexNumber;
         sIndexNumberToProposal[sIndexNumber] = proposal;
@@ -27,6 +30,9 @@ contract ProposalSystem {
         sIndexNumber++;
     }
 
+    /// Get list of all proposals along with index number
+    /// @return Index Number
+    /// @return Proposal
     function getAllProposals() external view returns (uint256[] memory, string[] memory) {
         return (sIndexNo, sProposals);
     }
